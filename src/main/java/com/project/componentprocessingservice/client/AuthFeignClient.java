1d35bcef3bb3ccd730ed0ad0576cabb0b835ea5a
GnaneswariK <105195493+GnaneswariK@users.noreply.github.com>
2022-05-25 21:42:50 +0530

    Update AuthFeignClient.java

--- src/main/java/com/project/componentprocessingservice/client/AuthFeignClient.java
+++ src/main/java/com/project/componentprocessingservice/client/AuthFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "authorization-service")
+@FeignClient(name = "872509-api-gateway-lb-940625491.us-east-1.elb.amazonaws.com")
 public interface AuthFeignClient {
 
     @GetMapping(value = "/auth/validate")
